cf92c833daf4380dc54d9a6354d3a36c0b155217
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-15 23:39:47 +0530

    Update admin.js

--- physcon,/js/admin.js
+++ physcon,/js/admin.js
@@ -1,1 +1,9 @@
+import { db } from "./firebase.js";
+import {
+  collection, getDocs
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+const snap = await getDocs(collection(db, "users"));
+snap.forEach(d => {
+  list.innerHTML += `<li>${d.data().email}</li>`;
+});
