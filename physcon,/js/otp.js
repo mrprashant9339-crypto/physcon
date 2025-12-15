11811ff1f2467f8ff701615f3d2d73d3ddc32278
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-16 00:14:35 +0530

    Update otp.js

--- physcon,/js/otp.js
+++ physcon,/js/otp.js
@@ -1,22 +1,13 @@
 import { auth } from "./firebase.js";
-import {
-  RecaptchaVerifier,
-  signInWithPhoneNumber
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { RecaptchaVerifier, signInWithPhoneNumber }
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-window.recaptchaVerifier = new RecaptchaVerifier(
-  auth,
-  'recaptcha',
-  { size: 'normal' }
-);
-
-window.sendOTP = async () => {
-  window.confirmationResult =
-    await signInWithPhoneNumber(auth, phone.value, window.recaptchaVerifier);
+window.recaptchaVerifier = new RecaptchaVerifier(auth,'recaptcha',{size:'normal'});
+window.sendOTP = async ()=>{
+  window.confirmationResult = await signInWithPhoneNumber(auth, phone.value, window.recaptchaVerifier);
   alert("OTP Sent");
 };
-
-window.verifyOTP = async () => {
+window.verifyOTP = async ()=>{
   await window.confirmationResult.confirm(otp.value);
-  window.location = "dashboard.html";
+  alert("Login Success"); location.href="dashboard.html";
 };
