7585c801b99f795ab7279fa8d07ed18ec5b8ab9e
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-15 23:36:51 +0530

    Update firebase.js

--- physcon,/js/firebase.js
+++ physcon,/js/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDVAJXcjhjXDUOTvCyv97a5M0N5vMOhd0g",
+  authDomain: "physcon-web.firebaseapp.com",
+  projectId: "physcon-web",
+  storageBucket: "physcon-web.firebasestorage.app",
+  messagingSenderId: "597364247516",
+  appId: "1:597364247516:web:52f3f489dc1ca092661d19"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
