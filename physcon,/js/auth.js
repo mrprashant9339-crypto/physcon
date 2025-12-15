8836e3e19733010ecc1c1e6546fd2ccbf7c0e3b4
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-16 00:14:11 +0530

    Update auth.js

--- physcon,/js/auth.js
+++ physcon,/js/auth.js
@@ -1,29 +1,16 @@
 import { auth, db } from "./firebase.js";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
-
-import {
-  doc, setDoc
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { doc, setDoc }
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 window.signup = async () => {
-  const email = email.value;
-  const password = password.value;
-
-  const res = await createUserWithEmailAndPassword(auth, email, password);
-
-  await setDoc(doc(db, "users", res.user.uid), {
-    email: email,
-    role: "user",
-    created: new Date()
-  });
-
-  window.location = "dashboard.html";
+  const res = await createUserWithEmailAndPassword(auth, email.value, password.value);
+  await setDoc(doc(db,"users",res.user.uid),{email:email.value,role:"user",created:new Date()});
+  alert("Signup Success"); location.href="dashboard.html";
 };
 
 window.login = async () => {
   await signInWithEmailAndPassword(auth, email.value, password.value);
-  window.location = "dashboard.html";
+  alert("Login Success"); location.href="dashboard.html";
 };
