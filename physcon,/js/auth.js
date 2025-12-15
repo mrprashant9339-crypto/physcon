3cd6d377daa3006d172b06fb073ba0349dd3b88c
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-15 23:37:39 +0530

    Update auth.js

--- physcon,/js/auth.js
+++ physcon,/js/auth.js
@@ -1,1 +1,29 @@
+import { auth, db } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+import {
+  doc, setDoc
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+window.signup = async () => {
+  const email = email.value;
+  const password = password.value;
+
+  const res = await createUserWithEmailAndPassword(auth, email, password);
+
+  await setDoc(doc(db, "users", res.user.uid), {
+    email: email,
+    role: "user",
+    created: new Date()
+  });
+
+  window.location = "dashboard.html";
+};
+
+window.login = async () => {
+  await signInWithEmailAndPassword(auth, email.value, password.value);
+  window.location = "dashboard.html";
+};
