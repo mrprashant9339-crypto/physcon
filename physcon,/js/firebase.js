fe0d044df63ecb41f68594aca4f1b935722df060
mrprashant9339-crypto <mrprashant9339@gmail.com>
2025-12-19 00:31:29 +0530

    Update firebase.js

--- physcon,/js/firebase.js
+++ physcon,/js/firebase.js
@@ -19,25 +19,3 @@ const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
 export { auth, googleProvider, githubProvider };
-<int>
-     import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider } 
-  from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDVAJXcjhjXDUOTvCyv97a5M0N5vMOhd0g",
-  authDomain: "physcon-web.firebaseapp.com",
-  projectId: "physcon-web",
-  storageBucket: "physcon-web.firebasestorage.app",
-  messagingSenderId: "597364247516",
-  appId: "1:597364247516:web:52f3f489dc1ca092661d19",
-  measurementId: "G-QEXKT8MF7Z"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const githubProvider = new GithubAuthProvider();
-
-export { auth, googleProvider, githubProvider };
-<int>
